7c7a70784a0eba09d048a500872241cb59956980
Lindrit14 <Lindrit_lindi@hotmail.com>
2023-04-16 16:41:34 +0200

    Initial Commit

---
+++ src/main/java/com/fhcampuswien/MuscleMate/MuscleMateApplication.java
@@ -0,0 +1,14 @@
+package com.fhcampuswien.MuscleMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuscleMateApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(MuscleMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fhcampuswien/MuscleMate/MuscleMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhcampuswien.MuscleMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuscleMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
