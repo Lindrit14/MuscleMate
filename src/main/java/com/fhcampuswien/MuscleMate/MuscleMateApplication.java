35daa9675c4c85c91c2ba92328fdf814d0cc8735
Lindrit14 <Lindrit_lindi@hotmail.com>
2023-05-22 21:26:18 +0200

    set up exercise model class and a path to display a test json for an exercise element

--- src/main/java/com/fhcampuswien/MuscleMate/MuscleMateApplication.java
+++ src/main/java/com/fhcampuswien/MuscleMate/MuscleMateApplication.java
@@ -1,15 +1,19 @@
 package com.fhcampuswien.MuscleMate;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
+
 public class MuscleMateApplication {
-
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
 		SpringApplication.run(MuscleMateApplication.class, args);
 	}
 
 }
+
+
+
--- src/main/java/com/fhcampuswien/MuscleMate/service/ExerciseService.java
+++ src/main/java/com/fhcampuswien/MuscleMate/service/ExerciseService.java
@@ -1,4 +1,24 @@
 package com.fhcampuswien.MuscleMate.service;
 
+import com.fhcampuswien.MuscleMate.model.Exercise;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ExerciseService {
+
+    //example of an exercise
+    public List<Exercise> getExercise(){
+        return List.of(
+                new Exercise(
+                        1,
+                        "Incline Hammer Curls",
+                        "biceps",
+                        "strength",
+                        "beginner",
+                        0
+                )
+        );
+    }
 }
