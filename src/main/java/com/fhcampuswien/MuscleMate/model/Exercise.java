9382eefcb79c6f3ad0016c8673043807a92887be
Lindrit14 <Lindrit_lindi@hotmail.com>
2023-05-24 09:22:36 +0200

    rewrite Model Exercise because of new API

--- src/main/java/com/fhcampuswien/MuscleMate/model/Exercise.java
+++ src/main/java/com/fhcampuswien/MuscleMate/model/Exercise.java
@@ -5,90 +5,77 @@ package com.fhcampuswien.MuscleMate.model;
 import java.util.Objects;
 public class Exercise {
 
+    private String bodyPart;
 
-    private Integer id;
+    private String equipment;
+
+    private String gifUrl;
+
+    private String id;
 
     private String name;
 
-    private String type;
-
-    private String muscle;
-    private String equipment;
-    private String difficulty;
-
-    private String instructions;
-
-
-    //Constructors
-    public Exercise(Integer id, String name, String type, String muscle, String difficulty, String equipment, String instructions ) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.muscle = muscle;
-        this.equipment = equipment;
-        this.difficulty = difficulty;
-        this.instructions = instructions;
-    }
+    private String target;
 
     public Exercise() {
+
     }
 
-    //Getters
-    public Integer getId() {
-        return id;
+    public Exercise(String bodyPart, String equipment, String gifUrl, String id, String name, String target) {
+        this.bodyPart = bodyPart;
+        this.equipment = equipment;
+        this.gifUrl = gifUrl;
+        this.id = id;
+        this.name = name;
+        this.target = target;
     }
 
-    public String getName() {
-        return name;
+    public String getBodyPart() {
+        return bodyPart;
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public String getMuscle() {
-        return muscle;
-    }
-
-    public String getDifficulty() {
-        return difficulty;
+    public void setBodyPart(String bodyPart) {
+        this.bodyPart = bodyPart;
     }
 
     public String getEquipment() {
         return equipment;
     }
 
-    public String getInstructions() {
-        return instructions;
+    public void setEquipment(String equipment) {
+        this.equipment = equipment;
     }
 
-    //Setters
-    public void setId(Integer id) {
+    public String getGifUrl() {
+        return gifUrl;
+    }
+
+    public void setGifUrl(String gifUrl) {
+        this.gifUrl = gifUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public String getTarget() {
+        return target;
     }
 
-    public void setMuscle(String muscle) {
-        this.muscle = muscle;
-    }
-
-    public void setDifficulty(String difficulty) {
-        this.difficulty = difficulty;
-    }
-
-    public void setEquipment(String equipment) {
-        this.equipment = equipment;
-    }
-
-    public void setInstructions(String instructions) {
-        this.instructions = instructions;
+    public void setTarget(String target) {
+        this.target = target;
     }
 
     @Override
@@ -96,23 +83,23 @@ public class Exercise {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Exercise exercise = (Exercise) o;
-        return Objects.equals(id, exercise.id) && Objects.equals(name, exercise.name) && Objects.equals(type, exercise.type) && Objects.equals(muscle, exercise.muscle) && Objects.equals(equipment, exercise.equipment) && Objects.equals(difficulty, exercise.difficulty) && Objects.equals(instructions, exercise.instructions);
+        return Objects.equals(bodyPart, exercise.bodyPart) && Objects.equals(equipment, exercise.equipment) && Objects.equals(gifUrl, exercise.gifUrl) && Objects.equals(id, exercise.id) && Objects.equals(name, exercise.name) && Objects.equals(target, exercise.target);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, type, muscle, equipment, difficulty, instructions);
+        return Objects.hash(bodyPart, equipment, gifUrl, id, name, target);
     }
+
 
     @Override
     public String toString() {
         return "Exercise{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", type='" + type + '\'' +
-                ", muscle='" + muscle + '\'' +
+                "bodyPart='" + bodyPart + '\'' +
                 ", equipment='" + equipment + '\'' +
-                ", difficulty='" + difficulty + '\'' +
+                ", id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", target='" + target + '\'' +
                 '}';
     }
 }
