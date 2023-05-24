5ac6460fbb310923b23f87e0afc7247798cfba30
Lindrit14 <Lindrit_lindi@hotmail.com>
2023-05-24 14:13:31 +0200

    changed design of exercise container

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -15,31 +15,38 @@ window.onload = function () {
             exercises.forEach((exercise) => {
 
                 const article = document.createElement("article")
+                const img = document.createElement("img")
                 const title = document.createElement("h1")
-                const type = document.createElement("p")
-                const muscle = document.createElement("p")
-                const difficulty = document.createElement("p")
+                const bodyPart = document.createElement("p")
+                const targetMuscle = document.createElement("p")
+                const equipment = document.createElement("p")
+                const imgDiv = document.createElement("div")
 
 
                 article.classList.add("exercise-article");
+                img
                 title.classList.add("exercise-title");
-                type.classList.add("exercise-info");
-                muscle.classList.add("exercise-info");
-                difficulty.classList.add("exercise-info");
+                bodyPart.classList.add("exercise-info");
+                targetMuscle.classList.add("exercise-info");
+                equipment.classList.add("exercise-info");
+                imgDiv.classList.add("imgContainer")
 
                 mainElement.append(article)
+                imgDiv.append(img)
+                article.append(imgDiv)
                 article.append(title)
-                article.append(type)
-                article.append(muscle)
-                article.append(difficulty)
+                article.append(bodyPart)
+                article.append(targetMuscle)
+                article.append(equipment)
 
 
 
 
+                img.src = exercise.gifUrl;
                 title.textContent = exercise.name;
-                type.textContent = "Type: " + exercise.type;
-                muscle.textContent = "Muscle: " + exercise.muscle;
-                difficulty.textContent = "Difficulty: " + exercise.difficulty;
+                bodyPart.textContent = "Body Part: " + exercise.bodyPart;
+                targetMuscle.textContent = "Targeted Muscle: " + exercise.target;
+                equipment.textContent = "Equipment: " + exercise.equipment;
             });
 
 
